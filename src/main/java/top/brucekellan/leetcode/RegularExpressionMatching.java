50e3e39ebbc94c90321060e98bc7d1cbcb3eb7c4
brucekellan <brucekellan@gmail.com>
2018-01-02 23:33:24 +0800

    leetcode 10. Regular Expression Matching

--- src/main/java/top/brucekellan/leetcode/RegularExpressionMatching.java
+++ src/main/java/top/brucekellan/leetcode/RegularExpressionMatching.java
@@ -23,7 +23,7 @@ package top.brucekellan.leetcode;
  */
 public class RegularExpressionMatching {
 
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch01(String s, String p) {
         if(p.isEmpty()) {
             return s.isEmpty();
         }
@@ -34,15 +34,51 @@ public class RegularExpressionMatching {
             if(s.isEmpty()) {
                 return false;
             }
-            return (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p.substring(1));
+            return (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') && isMatch01(s.substring(1), p.substring(1));
         }
         while(!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')) {
-            if(isMatch(s, p.substring(2))) {
+            if(isMatch01(s, p.substring(2))) {
                 return true;
             }
             s = s.substring(1);
         }
-        return isMatch(s, p.substring(2));
+        return isMatch01(s, p.substring(2));
     }
 
+    public boolean isMatch02(String s, String p) {
+        if(p.isEmpty()) {
+            return s.isEmpty();
+        }
+        if(p.length() > 1 && p.charAt(1) == '*') {
+            return isMatch02(s, p.substring(2))
+                    || (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'))
+                    && isMatch02(s.substring(1), p);
+        }
+        return !s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
+                && isMatch02(s.substring(1), p.substring(1));
+    }
+
+    public boolean isMatch(String s, String p) {
+        if (p.length() == 0) return s.length() == 0;
+        int sL = s.length(), pL = p.length();
+        boolean[][] dp = new boolean[sL + 1][pL + 1];
+        char[] sc = s.toCharArray(), pc = p.toCharArray();
+        dp[0][0] = true;
+        for (int i = 2; i <= pL; ++i) {
+            if (pc[i - 1] == '*' && dp[0][i - 2]) {
+                dp[0][i] = true;
+            }
+        }
+        for (int i = 1; i <= sL; ++i) {
+            for (int j = 1; j <= pL; ++j) {
+
+                if (pc[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] || (pc[j - 2] == sc[i - 1] || pc[j - 2] == '.') && dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && (pc[j - 1] == '.' || pc[j - 1] == sc[i - 1]);
+                }
+            }
+        }
+        return dp[sL][pL];
+    }
 }
